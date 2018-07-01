d9847807603094f78c16638f780bde7204f528fc
Savio Resende <savio@savioresende.com.br>
2018-07-01 14:16:51 +0000

    Added conditional to use https and configuration to work behind proxy.

--- examples/laravel/app/routes/web.php
+++ examples/laravel/app/routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+$proxy_url    = getenv('PROXY_URL');
+$proxy_schema = getenv('PROXY_SCHEMA');
+
+if (!empty($proxy_url)) {
+    URL::forceRootUrl($proxy_url);
+}
+
+if (!empty($proxy_schema)) {
+    URL::forceScheme('https');
+}
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
